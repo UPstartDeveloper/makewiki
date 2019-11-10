30ee9becc8a4dece85874df5636ffdd46c039edd
= <zainr7989@gmail.com>
2019-11-10 15:22:09 -0800

    [UPDATE] Make the Page slug field editable by users.

--- wiki/forms.py
+++ wiki/forms.py
@@ -6,4 +6,4 @@ class PageForm(forms.ModelForm):
     """ Render and process a form based on the Page model. """
     class Meta:
         model = Page
-        fields = ['title', 'author', 'content']
+        fields = ['title', 'author', 'content', 'slug']
